8a18d1a957d176ea49e2b55551e897cc42764a06
GKM <GKM@GKMLAPTOP>
2022-05-07 10:56:33 -0400

    try 5...

---
+++ src/test/java/git2/DemoFour.java
@@ -0,0 +1,14 @@
+package git2;
+
+public class DemoFour {
+	
+	public static void main(String[] args) {
+		System.out.println("Demo Four...");
+	}
+	
+	
+	public static void div (int a, int b) {
+		int result = a/b;
+		System.out.println(result);
+	}
+}
